411dd4b9aaed1d8eca2cada32927e86a37098516
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-04-29 15:48:15 +0300

    created post list in my run.py

--- run.py
+++ run.py
@@ -1,5 +1,20 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Dennis Mwaniki',
+        'title': 'Pitch Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2020'
+    },
+    {
+        'author': 'Chep Boyet',
+        'title': 'Pitch Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2020'
+    }
+]
 
 
 @app.route("/")
@@ -11,6 +26,7 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
