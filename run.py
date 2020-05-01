919d5e8d3eed29ac0f3aee7be57ca959349b1ffb
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-05-01 13:10:08 +0300

    created a login feature in my app

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY']= '6b5818f5898dfa22b76c4222a8d5dbe4'
 posts = [
     {
-        'author': 'Borey Schafer',
+        'author': 'Bobey gafer',
         'title': 'Pitch Post 1',
         'content': 'First post content',
         'date_posted': 'April 20, 2018'
@@ -20,8 +20,8 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def index():
-    return render_template('index.html', posts=posts)
+def home():
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
@@ -33,12 +33,19 @@ def register():
     form=RegistrationForm()
     if form.validate_on_submit():
         flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html',title='Register',form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    form=RegistrationForm()
-    return render_template('login.html',title='Login',form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
     
 if __name__ == '__main__':
     app.run(debug=True)
