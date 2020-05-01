de27e8faa10d8437d9af01ef65266e03ec7f0de0
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-05-01 13:23:12 +0300

    defined sqlalchemy url

--- run.py
+++ run.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template, url_for,flash,redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm,LoginForm
 app = Flask(__name__)
+
 app.config['SECRET_KEY']= '6b5818f5898dfa22b76c4222a8d5dbe4'
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'
+
+db=SQLAlchemy(app)
 posts = [
     {
         'author': 'Bobey gafer',
