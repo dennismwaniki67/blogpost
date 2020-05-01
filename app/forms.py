f35357ec6ea0cd0788366b46f276d25566c76555
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-05-01 20:06:51 +0300

    added some features for our login

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo,ValidationError
-
+from app.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -13,11 +13,11 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-    def validate_field(self,username):
+    def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That username is taken. Please choose another one')
-    
+            raise ValidationError('That username is taken. Please choose a different one.')
+
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
