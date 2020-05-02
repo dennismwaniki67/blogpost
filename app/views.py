6d223edd81f973a3330d7a2c37e03cabccc7014f
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-05-02 12:25:18 +0300

    created a delete post route

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for,flash,redirect,request
+from flask import render_template, url_for,flash,redirect,request,abort
 from app import app, db, bcrypt
 from app.forms import RegistrationForm,LoginForm,UpdateAccountForm,PostForm
 from app.models import User,Post
@@ -125,3 +125,14 @@ def update_post(post_id):
     return render_template('create_post.html', title='Update Post',
                            form=form, legend='Update Post')
 
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home')
