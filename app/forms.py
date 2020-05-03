19d09a6348fbdf75acdf9d83b56c3df9f829b75a
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-05-03 11:44:03 +0300

    created a resetform for our email

--- app/forms.py
+++ app/forms.py
@@ -64,3 +64,13 @@ class PostForm(FlaskForm):
 class CommentForm(FlaskForm):
     content = TextAreaField("Content", validators=[DataRequired()])
     submit = SubmitField("Comment")
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
